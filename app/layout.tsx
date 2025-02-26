1ce322bc81597b8fe35d4950fb358dde83401563
Shubham Bhat <bhatsupshubham@gmail.com>
2025-02-26 10:35:39 +0530

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./provider";
+import { Analytics } from '@vercel/analytics/next';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,6 +37,7 @@ export default function RootLayout({
       >
         <Providers>
          {children}
+         <Analytics />
         </Providers>
       </body>
     </html>
