9eb0805656f8d685a907962aaec9312ea9aabb58
Shubham Bhat <bhatsupshubham@gmail.com>
2025-03-06 23:59:45 +0530

    Revert "starting to add role based authentication"
    This reverts commit 3c706951304dec456243dd43381cd06df7f35686.

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,16 @@
 import { BackgroundBeams } from "@/components/ui/background-gradient-animation";
-import Link from 'next/link';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import Image from 'next/image';
-import {Button} from "@/components/ui/button";
-
+import LoginForm from "@/components/ui/LoginForm";
+import { Redirect } from "@/components/ui/Redirect";
 export default function Home() {
   return (
-    <div className="min-h-screen flex items-center justify-center overflow-hidden z-10 text-white">
-      <div className="h-70 flex justify-center items-center p-4 text-center bg-black border border-white rounded-lg">
-        <div className="flex flex-col items-center">
-          <Image src="/Untitled (7).png" alt="logo" width={250} height={250} />
-          
-        </div>
-        
-        <div className="space-x-4">
-        <h1 className="p-10">Select Your Role To Login!</h1>
-          
-          <Button asChild>
-            <Link href="./student-login">
-              Login as Student
-            </Link>
-          </Button>
-          <Button asChild>
-            <Link href="/admin-login">
-              Login as Admin
-            </Link>
-          </Button>
-          <Button asChild>
-            <Link href="/teacher-login">
-              Login as Teacher
-            </Link>
-          </Button>
-        </div>
-      </div>
-      <BackgroundBeams />
+    <div>
+      <Redirect/>
+    <div className="min-h-screen flex items-center justify-center overflow-hidden z-10 bg-black">
+    <LoginForm/>
+    </div>
+    <BackgroundBeams/>
     </div>
   );
 }
+
+
