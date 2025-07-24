b8286d91557747993d081d7045a3c7e7d86bdb16
Shubham Bhat <bhatsupshubham@gmail.com>
2025-07-25 01:09:47 +0530

    minor UI changes  LANDING PG

---
+++ components/ui/image-gallery.tsx
@@ -0,0 +1,16 @@
+// import { BackgroundBeams } from "@/components/ui/background-beams-with-collision";
+// import { BackgroundBeamsWithCollision } from "@/components/ui/background-beams-with-collision";
+// import LoginForm from "@/components/ui/LoginForm";
+// import { Redirect } from "@/components/ui/Redirect";
+import { Carousel } from "@material-tailwind/react";
+export default function ImageGallery() {
+  return (
+    <div className="border rounded-3xl">
+        <div className="w-full h-full">
+            IMAGE GALLERY
+        </div>
+    </div>
+  );
+}
+
+
