ffcca04101280dfe402372a551c44738d2907f94
Shubham Bhat <bhatsupshubham@gmail.com>
2025-07-25 22:03:52 +0530

    button color improv for submit

--- components/ui/LoginForm.tsx
+++ components/ui/LoginForm.tsx
@@ -41,26 +41,16 @@ Login to your account to view your Profile and Latest Notifications</p>
 
 
         <button
-          className="bg-[#243b55] relative group/btn w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+          className="bg-[#2e2d31] relative group/btn w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
           onClick={() => signIn()}
         >
           Sign In With Google &rarr;
-          <BottomGradient />
         </button>
 
         <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
         <div className="text-center text-blue-400 font-bold"><Link href="/teacher" >Click Here For Admin/Teacher Login!</Link></div>
       </form>
     </div>
-  )
-}
-
-const BottomGradient = () => {
-  return (
-    <>
-      <span className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent" />
-      <span className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent" />
-    </>
   )
 }
 
