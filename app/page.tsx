73a92955654c41eaeddcaa9a42f99ac6f3863ff3
Shubham Bhat <bhatsupshubham@gmail.com>
2025-07-25 13:00:26 +0530

    revamped ui for Login Page,added gallery carousel

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,13 @@ export default function Home() {
   return (
     <BackgroundBeamsWithCollision>
     <div className="w-screen h-screen flex justify-center items-center align-middle">
-    <div className="w-3/4 flex p-5 h-4/5 items-center rounded-3xl justify-between bg-[#2C2638] overflow-hidden">
-        <div className="">
+    <div className="w-3/4 flex p-5 h-4/5 align-middle items-center rounded-3xl justify  bg-[#2C2638] overflow-hidden">
+        <div className="w-1/2 overflow-hidden rounded-3xl flex items-center justify-center h-full">
           <ImageGallery/>
         </div>
-        <div className="">
+        <div className="w-1/2">
           <Redirect/>
-        <div className="min-h-screen flex items-center justify-center overflow-hidden z-10">
+        <div className=" flex items-center align-middle justify-center overflow-hidden z-10">
           <LoginForm/>
         </div>
         </div>
