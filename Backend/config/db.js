64ce03c8b21034b3fe07fc874a957adc67426189
om <rindheom43@gmail.com>
2025-07-31 16:44:20 +0530

    minor tweaks and fixes

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,20 +1,22 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
+// dbConnection.js
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 dotenv.config();
 
-const dbConnection = async () => {
+const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DBURL, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log("Connected to MongoDB successfully.");
-  } catch (err) {
-    console.error("Error connecting to MongoDB:", err.message);
-    process.exit(1); // Exit process with failure
+    console.log("✅ Connected to MongoDB");
+  } catch (error) {
+    console.error("❌ MongoDB connection error:", error.message);
+    process.exit(1);
   }
 };
 
-module.exports = dbConnection;
+module.exports = connectDB;
