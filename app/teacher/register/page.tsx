1f238188ae2cbf1fa95258c7c5bb0f02bf0139f5
Shubham Bhat <bhatsupshubham@gmail.com>
2025-08-02 23:22:27 +0530

    upgrading next/image to new image component

--- app/teacher/register/page.tsx
+++ app/teacher/register/page.tsx
@@ -56,6 +56,7 @@ export default function Home() {
   };
   console.log(errors);
   return (
+    // </BackgroundBeamsWithCollision>
     <div className="w-screen relative h-screen flex justify-center items-center align-middle">
       <Aurora
   colorStops={["#c94b4b", "#302b63", "#4b134f"]}
@@ -63,81 +64,79 @@ export default function Home() {
   amplitude={1.5}
   speed={0.5}
 />
-    <div className="font-figtree absolute w-3/4 z-20 backdrop-blur-md flex p-5 h-4/5 align-middle items-center rounded-3xl bg-[#6a69691e] overflow-hidden">
-        <div className="w-1/2 hidden md:flex overflow-hidden rounded-3xl items-center justify-center h-full">
-          <ImageGallery/>
-        </div>
-        <div className="w-full md:w-1/2">
-         
-          <div className="flex items-center align-middle justify-center overflow-hidden z-10">
-            <form 
-              className="flex w-full p-7 items-center flex-col gap-3 py-5 rounded-2xl "
-              onSubmit={handleSubmit(onSubmit)}
-            >
-              <p className="text-white text-center text-2xl">
-                Register as a Teacher at PDEA&apos;s Portal
-              </p>
-              <p>Already Registered as a Teacher?<Link className="text-blue-500" href="/teacher/login"> Login.</Link></p>
-              <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-2 h-[1px] w-full" />
+      <div className="font-figtree absolute w-3/4 z-20 backdrop-blur-md flex p-5 h-4/5 align-middle items-center rounded-3xl bg-[#6a69691e] overflow-hidden">
+          <div className="w-1/2 hidden md:flex overflow-hidden rounded-3xl items-center justify-center h-full">
+            <ImageGallery/>
+          </div>
+          <div className="w-full md:w-1/2">
+           
+            <div className="flex items-center align-middle justify-center overflow-hidden z-10">
+              <form 
+                className="flex w-full p-7 items-center flex-col gap-3 py-5 rounded-2xl "
+                onSubmit={handleSubmit(onSubmit)}
+              >
+                <p className="text-white text-center text-2xl">
+                  Register as a Teacher at PDEA&apos;s Portal
+                </p>
+                <p>Already Registered as a Teacher?<Link className="text-blue-500" href="/teacher/login"> Login.</Link></p>
+                <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-2 h-[1px] w-full" />
 
-              <LabelInputContainer>
-                <Label htmlFor="firstname">First name</Label>
-                <Input id="firstname" placeholder="Tyler" type="text" {...register("firstname", { required: true })} />
-              </LabelInputContainer>
+                <LabelInputContainer>
+                  <Label htmlFor="firstname">First name</Label>
+                  <Input id="firstname" placeholder="Tyler" type="text" {...register("firstname", { required: true })} />
+                </LabelInputContainer>
 
-              <LabelInputContainer>
-                <Label htmlFor="lastname">Last name</Label>
-                <Input id="lastname" placeholder="Durden" type="text" {...register("lastname", { required: true })} />
-              </LabelInputContainer>
+                <LabelInputContainer>
+                  <Label htmlFor="lastname">Last name</Label>
+                  <Input id="lastname" placeholder="Durden" type="text" {...register("lastname", { required: true })} />
+                </LabelInputContainer>
 
-              <LabelInputContainer>
-                <Label htmlFor="email">Email Address</Label>
-                <Input id="email" placeholder="projectmayhem@fc.com" type="email" {...register("email", { required: true })} />
-              </LabelInputContainer>
+                <LabelInputContainer>
+                  <Label htmlFor="email">Email Address</Label>
+                  <Input id="email" placeholder="projectmayhem@fc.com" type="email" {...register("email", { required: true })} />
+                </LabelInputContainer>
 
-              <LabelInputContainer>
-                <Label htmlFor="password">Password</Label>
-                <Input id="password" placeholder="••••••••" type="password" {...register("password", { required: true })} />
-              </LabelInputContainer>
+                <LabelInputContainer>
+                  <Label htmlFor="password">Password</Label>
+                  <Input id="password" placeholder="••••••••" type="password" {...register("password", { required: true })} />
+                </LabelInputContainer>
 
-              <DropdownMenu >
-      <DropdownMenuTrigger asChild>
-        <Button 
-        className="bg-gray-800 w-full mt-2 text-md" 
-        variant="secondary">
-          {Branch === "Branch" ? "Select Branch" : Branch}
-          <ChevronDown className="ml-2 h-5 w-5" />
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-full">
-        {/* <DropdownMenuSeparator /> */}
-        <DropdownMenuRadioGroup className="w-[480px]" value={Branch} onValueChange={setBranch}>
-          <DropdownMenuRadioItem value="Information Technology">Information Technology</DropdownMenuRadioItem>
-          <DropdownMenuRadioItem value="Computer Science">Computer Science</DropdownMenuRadioItem>
-          <DropdownMenuRadioItem value="Mechanical">Mechanical</DropdownMenuRadioItem>
-          <DropdownMenuRadioItem value="AI/DS">AI/DS</DropdownMenuRadioItem>
-          <DropdownMenuRadioItem value="MCA">MCA</DropdownMenuRadioItem>
-        </DropdownMenuRadioGroup>
-      </DropdownMenuContent>
-    </DropdownMenu>
+                <DropdownMenu >
+        <DropdownMenuTrigger asChild>
+          <Button 
+          className="bg-gray-800 w-full mt-2 text-md" 
+          variant="secondary">
+            {Branch === "Branch" ? "Select Branch" : Branch}
+            <ChevronDown className="ml-2 h-5 w-5" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-full">
+          {/* <DropdownMenuSeparator /> */}
+          <DropdownMenuRadioGroup className="w-[480px]" value={Branch} onValueChange={setBranch}>
+            <DropdownMenuRadioItem value="Information Technology">Information Technology</DropdownMenuRadioItem>
+            <DropdownMenuRadioItem value="Computer Science">Computer Science</DropdownMenuRadioItem>
+            <DropdownMenuRadioItem value="Mechanical">Mechanical</DropdownMenuRadioItem>
+            <DropdownMenuRadioItem value="AI/DS">AI/DS</DropdownMenuRadioItem>
+            <DropdownMenuRadioItem value="MCA">MCA</DropdownMenuRadioItem>
+          </DropdownMenuRadioGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
 
-              <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-1 h-[1px] w-full" />
+                <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-1 h-[1px] w-full" />
 
-              <input
-                className="bg-[#443379] text-lg text-white rounded-lg cursor-pointer hover:bg-black transition w-full h-10"
-                type="submit"
-                value="Register"
+                <input
+                  className="bg-[#443379] text-lg text-white rounded-lg cursor-pointer hover:bg-black transition w-full h-10"
+                  type="submit"
+                  value="Register"
+                  
+                />
                 
-              />
-              
-        <div className="text-center text-lg text-blue-500 font-figtree"><Link href="/" >Click Here For Student Login!</Link></div>
-            </form>
+          <div className="text-center text-lg text-blue-500 font-figtree"><Link href="/" >Click Here For Student Login!</Link></div>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
     </div>
-
-    // </BackgroundBeamsWithCollision>
   );
 }
 const LabelInputContainer = ({
